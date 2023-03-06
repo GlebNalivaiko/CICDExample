754f1b15abdc6a049de2f11c52b669c0f8638873
Глеб <gleb.gol2014@yandex.ru>
2023-03-06 19:41:55 +0300

    init commit

---
+++ src/test/java/by/nalivaiko/cicdexample/CicdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nalivaiko.cicdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
